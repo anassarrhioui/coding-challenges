2c3428275744c3db47f99f0fb6d142b6387a4c3b
anassarrhioui <anassarrhioui@gmail.com>
2022-01-24 00:16:43 +0100

    Odd Occurrence Integer Refactor

--- OddOccurrenceInteger.java
+++ OddOccurrenceInteger.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class OddOccurrenceInteger {
     public static void main(String[] args) {
-        int result = findIt(new int[]{1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1}); //5
+        int result = findIt(new int[]{1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1}); // 10
         System.out.println(result);
     }
 
@@ -13,25 +13,22 @@ public class OddOccurrenceInteger {
         List<Integer> targetList = new ArrayList<>();
         Arrays.stream(tab).forEach(targetList::add);
         Collections.sort(targetList);
-        System.out.println(targetList.toString());
         Stack<Integer> stack = new Stack<>();
 
-        for (int element : targetList) {
+        for (int element : targetList)
             if (stack.isEmpty()) {
                 stack.push(element);
-            } else {
-                if (stack.peek() == element) {
+            } else
+                if (stack.peek() == element)
                     stack.push(element);
-                } else {
+                else
                     if (stack.size() % 2 == 1)
                         return stack.peek();
                     else {
                         stack.clear();
                         stack.push(element);
                     }
-                }
-            }
-        }
+
         return stack.peek(); // Odd found in the last
     }
 }
